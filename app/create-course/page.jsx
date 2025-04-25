b400003709179c888789205071ab57d27dadac3d
shashidhar k <160392735+Pluggycode@users.noreply.github.com>
2025-04-25 21:15:33 +0530

    Update page.jsx

--- app/create-course/page.jsx
+++ app/create-course/page.jsx
@@ -117,11 +117,11 @@ const CreateCourse = () => {
                 {StepperOption.map((item,index)=>(
                     <div className="flex items-center">
                         <div className="flex flex-col items-center w-[150px] md:w-[100px] ">
-                        <div className={`bg-gray-200 p-3 rounded-full text-white ${activeIndex>=index&&'bg-green-400'}`} >{item.icon}</div>
+                        <div className={`bg-gray-200 p-3 rounded-full text-white ${activeIndex>=index&&'bg-purple-300'}`} >{item.icon}</div>
                         <h2 className={`hidden md:block md:text-sm`}>{item.name}</h2>
                     </div>
                    {index!=StepperOption?.length-1 &&<div className={`h-1 w-[50px] md:w-[100px] rounded-full lg:w-[170px] bg-gray-300
-                    ${activeIndex>index &&  'bg-green-400'}`}></div>}
+                    ${activeIndex>index &&  'bg-purple-300'}`}></div>}
                     </div>
 
                 ))}
