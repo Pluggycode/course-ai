bed5b498263d6321352ae2e91dbf4cf3d9317184
shashidhar k <160392735+Pluggycode@users.noreply.github.com>
2025-04-26 09:11:53 +0530

    Update SideBar.jsx

--- app/dashboard/_components/SideBar.jsx
+++ app/dashboard/_components/SideBar.jsx
@@ -49,9 +49,8 @@ function SideBar() {
       <ul>
         {Menu.map((item,index)=>(
           <Link href={item.path}>
-          <div className={`flex items-center gap-2 text-gray-500 p-3 cursor-pointer hover:bg-gray-300 te rounded-lg mb-2 border-white
-          ${item.path==path && 'bg-blue-500 bg-gradient-to-br from-primary to-blue-300
-    bg-[length:200%_200%] animate-gradient-move  text-white'}`}>
+            <div className={`flex items-center gap-2 text-gray-500 p-3 cursor-pointer hover:bg-gray-300 te rounded-lg mb-2 border-white
+          ${item.path==path && 'bg-gradient-to-br from-green-400 to-blue-400 bg-[length:200%_200%] animate-gradient-move  text-white'}`}>
             <h2 className='text-center ml-15'>{item.name}</h2>
           </div>
           </Link>
