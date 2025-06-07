1772eceb4b7c54f4f4ed40e837c00a0fb213b9eb
shashidhar k <160392735+Pluggycode@users.noreply.github.com>
2025-06-07 12:59:25 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { clerkMiddleware,createRouteMatcher } from "@clerk/nextjs/server";
 
 
 const isProtectedRoute = createRouteMatcher(
-  ['/dashboard(.*)','/create-course(.*)'])
+  ['/dashboard(.*)','/create-course(.*)','/dashboard/explore(.*)'])
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect()
